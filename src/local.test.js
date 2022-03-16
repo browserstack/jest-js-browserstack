57fd8df2f0b1807e1bee4d4016a49a5223c13509
Dhruvil Shah <dsdruvil8@gmail.com>
2022-03-16 21:51:32 +0530

    refactoring

--- src/local.test.js
+++ src/local.test.js
@@ -3,80 +3,80 @@ const browserstackLocal = require("browserstack-local");
 const https = require("https");
 
 describe("BStack demo test", () => {
-	let driver,
-		statusFail = true,
-		local = new browserstackLocal.Local();
+  let driver;
+  let statusFail = true;
+  let local = new browserstackLocal.Local();
 
-	beforeEach(async () => {
-		const username = process.env.BROWSERSTACK_USERNAME || "",
-			accessKey = process.env.BROWSERSTACK_ACCESS_KEY || "";
+  beforeEach(async () => {
+    const username = process.env.BROWSERSTACK_USERNAME || "";
+    const accessKey = process.env.BROWSERSTACK_ACCESS_KEY || "";
 
-		await new Promise((res) => {
-			local.start(
-				{
-					key: accessKey,
-					verbose: true,
-					logFile: "./local.log",
-				},
-				res
-			);
-		});
+    await new Promise((res) => {
+      local.start(
+        {
+          key: accessKey,
+          verbose: true,
+          logFile: "./local.log",
+        },
+        res
+      );
+    });
 
-		let capabilities = {
-			os: "Windows",
-			os_version: "10",
-			browserName: "Chrome",
-			browser_version: "latest",
-			project: "BStack Demo",
-			build: "jest-browserstack",
-			name: "local test",
-			"browserstack.local": true,
-			"browserstack.selenium_version": "3.6.0",
-			"browserstack.user": username,
-			"browserstack.key": accessKey,
-		};
+    let capabilities = {
+      os: "Windows",
+      os_version: "10",
+      browserName: "Chrome",
+      browser_version: "latest",
+      project: "BStack Demo",
+      build: "jest-browserstack",
+      name: "local test",
+      "browserstack.local": true,
+      "browserstack.selenium_version": "3.6.0",
+      "browserstack.user": username,
+      "browserstack.key": accessKey,
+    };
 
-		driver = await new Builder()
-			.usingServer("https://hub-cloud.browserstack.com/wd/hub")
-			.withCapabilities(capabilities)
-			.usingHttpAgent(
-				new https.Agent({
-					keepAlive: true,
-					keepAliveMsecs: 1000000,
-				})
-			)
-			.build();
+    driver = await new Builder()
+      .usingServer("https://hub-cloud.browserstack.com/wd/hub")
+      .withCapabilities(capabilities)
+      .usingHttpAgent(
+        new https.Agent({
+          keepAlive: true,
+          keepAliveMsecs: 1000000,
+        })
+      )
+      .build();
 
-		await driver.manage().window().maximize();
-	}, 100000);
+    await driver.manage().window().maximize();
+  }, 100000);
 
-	test("local test", async () => {
-		await driver.get("http://bs-local.com:45691/check");
+  test("local test", async () => {
+    await driver.get("http://bs-local.com:45691/check");
 
-		expect(await driver.findElement(By.css("body")).getText()).toBe(
-			"Up and running"
-		);
+    expect(await driver.findElement(By.css("body")).getText()).toBe(
+      "Up and running"
+    );
 
-		statusFail = false;
-	}, 10000000);
+    statusFail = false;
+  }, 10000000);
 
-	afterEach(async () => {
-		if (statusFail) {
-			await driver.executeScript(
-				`browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": ""}}`
-			);
-		} else {
-			await driver.executeScript(
-				`browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": ""}}`
-			);
-		}
-		if (driver) {
-			await driver.quit();
-		}
-		if (local) {
-			await new Promise((res) => {
-				local.stop(res);
-			});
-		}
-	}, 100000);
+  afterEach(async () => {
+    if (statusFail) {
+      await driver.executeScript(
+        `browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": ""}}`
+      );
+    } else {
+      await driver.executeScript(
+        `browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": ""}}`
+      );
+    }
+    if (driver) {
+      await driver.quit();
+    }
+    if (local) {
+      await new Promise((res) => {
+        local.stop(res);
+      });
+    }
+  }, 100000);
 });
--- src/single.test.js
+++ src/single.test.js
@@ -2,79 +2,79 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const https = require("https");
 
 describe("BStack demo test", () => {
-	let driver,
-		statusFail = true;
+  let driver;
+  let statusFail = true;
 
-	const username = process.env.BROWSERSTACK_USERNAME || "",
-		accessKey = process.env.BROWSERSTACK_ACCESS_KEY || "";
+  const username = process.env.BROWSERSTACK_USERNAME || "";
+  const accessKey = process.env.BROWSERSTACK_ACCESS_KEY || "";
 
-	beforeEach(async () => {
-		let capabilities = {
-			os: "Windows",
-			os_version: "11",
-			browserName: "Chrome",
-			browser_version: "latest",
-			project: "BStack Demo",
-			build: "jest-browserstack",
-			name: "single test",
-			"browserstack.local": "false",
-			"browserstack.selenium_version": "3.6.0",
-			"browserstack.user": username,
-			"browserstack.key": accessKey,
-		};
+  beforeEach(async () => {
+    let capabilities = {
+      os: "Windows",
+      os_version: "11",
+      browserName: "Chrome",
+      browser_version: "latest",
+      project: "BStack Demo",
+      build: "jest-browserstack",
+      name: "single test",
+      "browserstack.local": "false",
+      "browserstack.selenium_version": "3.6.0",
+      "browserstack.user": username,
+      "browserstack.key": accessKey,
+    };
 
-		driver = await new Builder()
-			.usingServer("https://hub-cloud.browserstack.com/wd/hub")
-			.withCapabilities(capabilities)
-			.usingHttpAgent(
-				new https.Agent({
-					keepAlive: true,
-					keepAliveMsecs: 1000000,
-				})
-			)
-			.build();
+    driver = await new Builder()
+      .usingServer("https://hub-cloud.browserstack.com/wd/hub")
+      .withCapabilities(capabilities)
+      .usingHttpAgent(
+        new https.Agent({
+          keepAlive: true,
+          keepAliveMsecs: 1000000,
+        })
+      )
+      .build();
 
-		await driver.manage().window().maximize();
-	}, 100000);
+    await driver.manage().window().maximize();
+  }, 100000);
 
-	test("login test", async () => {
-		await driver.get("https://bstackdemo.com");
+  test("login test", async () => {
+    await driver.get("https://bstackdemo.com");
 
-		await driver.findElement(By.css("#signin")).click();
+    await driver.findElement(By.css("#signin")).click();
 
-		await driver.wait(until.elementLocated(By.css("#username input")));
+    await driver.wait(until.elementLocated(By.css("#username input")));
 
-		await driver
-			.findElement(By.css("#username input"))
-			.sendKeys("locked_user", Key.ENTER);
+    await driver
+      .findElement(By.css("#username input"))
+      .sendKeys("locked_user", Key.ENTER);
 
-		await driver
-			.findElement(By.css("#password input"))
-			.sendKeys("testingisfun99", Key.ENTER);
+    await driver
+      .findElement(By.css("#password input"))
+      .sendKeys("testingisfun99", Key.ENTER);
 
-		await driver.findElement(By.css("#login-btn")).click();
+    await driver.findElement(By.css("#login-btn")).click();
 
-		await driver.wait(until.elementLocated(By.css(".api-error")));
+    await driver.wait(until.elementLocated(By.css(".api-error")));
 
-		expect(await driver.findElement(By.css(".api-error")).getText()).toBe(
-			"Your account has been locked."
-		);
+    expect(await driver.findElement(By.css(".api-error")).getText()).toBe(
+      "Your account has been locked."
+    );
 
-		statusFail = false;
-	}, 10000000);
+    statusFail = false;
+  }, 10000000);
 
-	afterEach(async () => {
-		if (statusFail) {
-			await driver.executeScript(
-				`browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": ""}}`
-			);
-		} else {
-			await driver.executeScript(
-				`browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": ""}}`
-			);
-		}
-		if (driver) {
-			await driver.quit();
-		}
-	}, 100000);
+  afterEach(async () => {
+    if (statusFail) {
+      await driver.executeScript(
+        `browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": ""}}`
+      );
+    } else {
+      await driver.executeScript(
+        `browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": ""}}`
+      );
+    }
+    if (driver) {
+      await driver.quit();
+    }
+  }, 100000);
 });
