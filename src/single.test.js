380583fe9d4620d1637ef167adff0935aa58d392
Dhruvil Shah <dsdruvil8@gmail.com>
2022-05-06 21:27:03 +0530

    updating test scripts

--- src/single.test.js
+++ src/single.test.js
@@ -1,6 +1,6 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
-const https = require("https");
+const { Builder, By, until } = require("selenium-webdriver");
 const capabilities = require("../conf/single.conf");
+const https = require("https");
 
 describe("BStack demo test", () => {
   let driver;
@@ -21,28 +21,28 @@ describe("BStack demo test", () => {
     await driver.manage().window().maximize();
   }, 100000);
 
-  test("login test", async () => {
-    await driver.get("https://bstackdemo.com");
+  test("add items to cart", async () => {
+    await driver.get("https://bstackdemo.com/");
+    await driver.wait(until.titleMatches(/StackDemo/i), 10000);
 
-    await driver.findElement(By.css("#signin")).click();
-
-    await driver.wait(until.elementLocated(By.css("#username input")));
-
-    await driver
-      .findElement(By.css("#username input"))
-      .sendKeys("locked_user", Key.ENTER);
-
-    await driver
-      .findElement(By.css("#password input"))
-      .sendKeys("testingisfun99", Key.ENTER);
-
-    await driver.findElement(By.css("#login-btn")).click();
-
-    await driver.wait(until.elementLocated(By.css(".api-error")));
-
-    expect(await driver.findElement(By.css(".api-error")).getText()).toBe(
-      "Your account has been locked."
-    );
+    // locating product on webpage and getting name of the product
+    let productText = await driver
+      .findElement(By.xpath('//*[@id="1"]/p'))
+      .getText();
+    // clicking the 'Add to cart' button
+    await driver.findElement(By.xpath('//*[@id="1"]/div[4]')).click();
+    // waiting until the Cart pane has been displayed on the webpage
+    driver.findElement(By.className("float-cart__content"));
+    // locating product in cart and getting name of the product in cart
+    let productCartText = await driver
+      .findElement(
+        By.xpath(
+          '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]'
+        )
+      )
+      .getText();
+    // checking whether product has been added to cart by comparing product name
+    expect(productText).toBe(productCartText);
 
     statusFail = false;
   }, 10000000);
